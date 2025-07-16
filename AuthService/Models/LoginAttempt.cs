03514b71a3e8b09bd11581f7153c5e0f943e047d
Viktor <kassov@ukr.net>
2025-07-16 10:52:29 +0300

    LoginAttempt

---
+++ AuthService/Models/LoginAttempt.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthService.Models
+{
+    public class LoginAttempt
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid? UserId { get; set; }
+        public string? UsernameAttempted { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string? IpAddress { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string? Reason { get; set; }
+    }
+
+}
