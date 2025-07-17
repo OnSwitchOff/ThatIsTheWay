b01629e18dbc4f525fd5b38a292b29553f1fc7fd
Viktor <kassov@ukr.net>
2025-07-17 16:38:46 +0300

    Remove Kestrel port configuration
    This commit removes the explicit configuration of Kestrel to listen on ports 8080 (HTTP) and 8081 (HTTPS). The `ConfigureKestrel` method and its associated options have been deleted. The code still sets up the database context and configures lockout settings using the options pattern.

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -11,13 +11,6 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Explicitly configure Kestrel to listen on 8080 (HTTP) and 8081 (HTTPS)
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(8080); // HTTP
-    options.ListenAnyIP(8081, listenOptions => listenOptions.UseHttps()); // HTTPS
-});
-
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
